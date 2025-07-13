9348d5887832b2b133241bd5a3d7cc3b3f4b967d
Chackochan Jose <127332816+Codesandmore@users.noreply.github.com>
2025-07-13 23:06:02 +0530

    Create 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,3 @@
-hand = {1:'Thumb',2:'Index Finger',3:'Middle Finger',4:'Ring Finger',0:'Pinky Finger'}
+hand={1:'Thumb',2:'Index Finger',3:'Middle Finger',4:'Ring Finger',0:'Pinky Finger'}
 n = int(input())
 print(hand[n%5])
